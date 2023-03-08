b4259de555075a037d50c7b1f4d78a0902747d3e
ghps2704 <100978223+ghps2704@users.noreply.github.com>
2023-03-08 20:20:43 -0300

    ProblemSet2_2.java

---
+++ Art.java
@@ -0,0 +1,17 @@
+public class Art {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        System.out.println("        _____");
+        System.out.println("      /       \\");
+        System.out.println("     /    /\\   \\");
+        System.out.println("    /    /  \\   \\");
+        System.out.println("   /    /    \\   \\");
+        System.out.println("  /    /      \\   \\");
+        System.out.println(" /____/________\\   \\");
+        System.out.println(" \\                  /");
+        System.out.prinln("  \\   JavaScript   /");
+        System.out.println("   \\              /");
+        System.out.println("    \\____________/");
+    } 
+}
